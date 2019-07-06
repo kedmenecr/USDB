0d3c877a53bf4e09df726ca7cedd93f88034579b
root <kedmenec.roberto@gmail.com>
2019-07-06 14:36:00 +0200

    modified:   deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ chmod 600 id_rsa # Allow read access to the private key
 ssh-add id_rsa # Add the private key to SSH
 
 git config --global push.default matching
-git remote add deploy ssh://git@$IP:$PORT$DEPLOY_DIR
+git remote add deploy ssh://root@$IP:$PORT$DEPLOY_DIR
 git push deploy master
 
 # Skip this command if you don't need to execute any additional commands after deploying.
